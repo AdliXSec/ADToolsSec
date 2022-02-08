e06059452571d6a0fe6b7fc503fc6b37fcbdda0c
AdliXSec <naufalsyahruradli@gmail.com>
2022-02-08 12:07:47 +0700

    add ipgeo

--- isi.py
+++ isi.py
@@ -3,6 +3,8 @@ import pyautogui as spam
 import urllib.request
 import urllib.parse
 import youtube_dl
+import requests
+import json
 from googletrans import Translator
 from pytube import YouTube
 from datetime import datetime
@@ -346,3 +348,31 @@ def ytmp3():
         ydl.download([link])
 
     print("Download success")
+
+def ipgeo():
+    os.system(hapus)
+    print('''
+     _____ _____     _____            _                     _   _             
+    |_   _|  __ \   / ____|          | |                   | | (_)            
+      | | | |__) | | |  __  ___  ___ | |     ___   ___ __ _| |_ _  ___  _ __  
+      | | |  ___/  | | |_ |/ _ \/ _ \| |    / _ \ / __/ _` | __| |/ _ \| '_ \ 
+     _| |_| |      | |__| |  __/ (_) | |___| (_) | (_| (_| | |_| | (_) | | | |
+    |_____|_|       \_____|\___|\___/|______\___/ \___\__,_|\__|_|\___/|_| |_|
+                    By : AdliXSec   Team : Dark Clown Security     
+                                                                
+    ''')
+    ipaddr = input("Ip Address : ")
+    ipreq = requests.get(f"http://ip-api.com/json/{ipaddr}")
+
+    if ipreq.status_code == 200:
+        ipdata = json.loads(ipreq.text)
+
+        if ipdata["status"] == "success":
+            for key in ipdata:
+                print(f"{key.capitalize()} : {ipdata[key]}")
+                
+                if key == "lon":
+                    lat = ipdata["lat"]
+                    lon = ipdata["lon"]
+                    maps = f"https://www.google.com/maps/@{lat},{lon},9z"
+                    print(f"Maps : {maps}")
